3469624a1a60bd4b7ec96ebb1322a1450f48f89a
pickzelz <pickzelz@gmail.com>
2018-04-26 11:48:32 +0700

    - add script

---
+++ SceneManagement/SceneManagementWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+public class SceneManagementWindow : EditorWindow {
+
+    [SerializeField] TreeViewState m_TreeViewState;
+    SceneView m_SimpleTreeView;
+
+    private void OnEnable()
+    {
+        if (m_TreeViewState == null)
+            m_TreeViewState = new TreeViewState();
+
+        m_SimpleTreeView = new SceneView(m_TreeViewState);
+    }
+
+    void OnGUI()
+    {
+
+        m_SimpleTreeView.OnGUI(new Rect(0, 0, position.width, position.height));
+    }
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/Scene Management")]
+    static void ShowWindow()
+    {
+        // Get existing open window or if none, make a new one:
+        var window = GetWindow<SceneManagementWindow>();
+        window.titleContent = new GUIContent("Scenes");
+        window.Show();
+    }
+}
---
+++ SceneManagement/SceneView.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+
+public class SceneView : TreeView{
+
+    List<SaveItem> allSaveitem;
+
+    public class SaveItem
+    {
+        public TreeViewItem item;
+        public string path;
+    }
+
+
+    public SceneView(TreeViewState treeViewState)
+        : base(treeViewState)
+    {
+        Reload();
+    }
+
+    protected override TreeViewItem BuildRoot()
+    {
+        // BuildRoot is called every time Reload is called to ensure that TreeViewItems 
+        // are created from data. Here we create a fixed set of items. In a real world example,
+        // a data model should be passed into the TreeView and the items created from the model.
+
+        // This section illustrates that IDs should be unique. The root item is required to 
+        // have a depth of -1, and the rest of the items increment from that.
+        var root = new TreeViewItem { id = 0, depth = -1, displayName = "Scenes" };
+
+        List<TreeViewItem> allItems = new List<TreeViewItem>();
+        allSaveitem = new List<SaveItem>();
+        int ids = 1;
+        foreach(var scene in EditorBuildSettings.scenes)
+        {
+            string path = scene.path;
+            string[] explode = path.Split('/');
+            var name = "";
+            if(explode.Length > 0)
+            {
+                name = explode[explode.Length - 1].Replace(".unity", "");
+            }
+
+            SaveItem saveitem = new SaveItem();
+            saveitem.item = new TreeViewItem { id = ids, depth = 1, displayName = name };
+            saveitem.path = path;
+
+            allItems.Add(saveitem.item);
+            allSaveitem.Add(saveitem);
+            ids++;
+        }
+        // Utility method that initializes the TreeViewItem.children and .parent for all items.
+        SetupParentsAndChildrenFromDepths(root, allItems);
+
+        // Return root of the tree
+        return root;
+    }
+
+    protected override void DoubleClickedItem(int id)
+    {
+        base.DoubleClickedItem(id);
+
+        SaveItem item = allSaveitem.Find(x => x.item.id == id);
+        EditorSceneManager.OpenScene(item.path);
+    }
+}
